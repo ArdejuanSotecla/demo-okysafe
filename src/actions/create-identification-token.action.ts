11b154129089515fdde1214afa3ee2f8733768b7
ArdejuanSotecla <arturo.dejuan@sotecla.com>
2025-03-03 12:53:26 +0100

    🩹 Cache token

--- src/actions/create-identification-token.action.ts
+++ src/actions/create-identification-token.action.ts
@@ -1,14 +1,43 @@
 "use server";
 
 import { env } from "@/lib/env";
-import { sign } from "jsonwebtoken";
+import { sign, decode } from "jsonwebtoken";
+
+let token: string | null = null;
 
 export const createIdentificationTokenAction = async (): Promise<string> => {
-  return sign(
-    {
-      clientPublicKey: env.NEXT_PUBLIC_OKYSAFE_CLIENT_PUBLIC_KEY,
-      clientWebOrigin: env.NEXT_PUBLIC_ORIGIN,
-    },
-    env.OKYSAFE_CLIENT_SECRET_KEY
-  );
+  if (token === null) {
+    token = sign(
+      {
+        clientPublicKey: env.NEXT_PUBLIC_OKYSAFE_CLIENT_PUBLIC_KEY,
+        clientWebOrigin: env.NEXT_PUBLIC_ORIGIN,
+      },
+      env.OKYSAFE_CLIENT_SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+  } else {
+    const decoded = decode(token);
+
+    if (
+      typeof decoded === "object" &&
+      decoded != null &&
+      decoded.exp != null &&
+      decoded.exp < Date.now() / 1000
+    ) {
+      token = sign(
+        {
+          clientPublicKey: env.NEXT_PUBLIC_OKYSAFE_CLIENT_PUBLIC_KEY,
+          clientWebOrigin: env.NEXT_PUBLIC_ORIGIN,
+        },
+        env.OKYSAFE_CLIENT_SECRET_KEY,
+        {
+          expiresIn: "1h",
+        }
+      );
+    }
+  }
+
+  return token;
 };
